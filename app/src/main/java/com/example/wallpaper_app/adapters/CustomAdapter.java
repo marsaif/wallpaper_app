b0440a3470ae244e59e405c21b874c0afec7dd26
saif <marzouga.saif@gmail.com>
2021-08-24 13:26:57 +0100

    implement endless list

--- app/src/main/java/com/example/wallpaper_app/adapters/CustomAdapter.java
+++ app/src/main/java/com/example/wallpaper_app/adapters/CustomAdapter.java
@@ -10,21 +10,34 @@ import android.widget.ImageView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.bumptech.glide.Glide;
 import com.example.wallpaper_app.R;
 import com.example.wallpaper_app.models.Wallpaper;
+import com.example.wallpaper_app.network.VolleySingleton;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder> {
 
     private List<Wallpaper> list ;
     private Context context;
+    private int page ;
 
     public CustomAdapter(List<Wallpaper> list, Context context) {
         this.list = list;
         this.context = context;
-
+        page = 1 ;
     }
 
     @NonNull
@@ -40,6 +53,12 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, @SuppressLint("RecyclerView") int position) {
         Glide.with(context).load(list.get(position).getUrlImage()).into(holder.getImageViewWallpaper()) ;
+
+        if (position == getItemCount() - 1)
+        {
+            page = page + 1 ;
+            requestNextWallpapers();
+        }
     }
 
     @Override
@@ -59,5 +78,58 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
         public ImageView getImageViewWallpaper() {
             return imageViewWallpaper;
         }
+    }
+
+
+
+    public void requestNextWallpapers()
+    {
+
+        String url = "https://api.pexels.com/v1/curated?page="+page+"&per_page=15" ;
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray jsonArray = response.getJSONArray("photos") ;
+                    for (int i = 0 ; i < jsonArray.length() ; i++)
+                    {
+                        JSONObject jsonObject = jsonArray.getJSONObject(i) ;
+
+                        int id = jsonObject.getInt("id") ;
+                        String photographer = jsonObject.getString("photographer");
+
+                        jsonObject = jsonObject.getJSONObject("src") ;
+
+                        String urlImage = jsonObject.getString("portrait") ;
+
+                        Wallpaper wallpaper = new Wallpaper(id,photographer,urlImage) ;
+                        list.add(wallpaper) ;
+                    }
+
+                    notifyDataSetChanged();
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("ffff ");
+
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String,String> map = new HashMap<>() ;
+                map.put("Authorization",context.getString(R.string.api_key)) ;
+                return map ;
+            }
+        };
+
+
+        VolleySingleton volleySingleton = VolleySingleton.getInstance(context) ;
+        volleySingleton.getRequestQueue().add(jsonObjectRequest);
+
     }
 }
